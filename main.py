8c8d715bf3a8f3fddce5873cadd54a7707515966
Ali Manzer <ali.iman.manzer@ulb.be>
2023-04-17 11:53:26 +0200

    data analysis

--- main.py
+++ main.py
@@ -1,29 +1,79 @@
 import requests
+import os
+import xml.etree.ElementTree as ET
+import sumolib
+import json
 import pandas as pd
+from shapely.geometry import Point
 
-import sys
-print(sys.version)
+API_KEY = os.getenv('API_KEY')
 
-data = requests.get(
-            f"http://data.mobility.brussels/traffic/api/counts/?request=devices&outputFormat=json&interval={15}")
+class SimulationResults:
+    endpoint = 'https://maps.googleapis.com/maps/api/distancematrix/json'
 
-columns = ['id', 'lat', 'lon']
-df_out = pd.DataFrame(columns=columns)
-all_data = data.json()['features']
+    def __init__(self) -> None:
+        net = sumolib.net.readNet('ulb/ulb.net.xml')
+        data_trips = self.read_trips()
 
-#print(all_data)
+    
+    def read_trips(self):
+        tree = ET.parse('ulb/trips.trips.xml')
+        root = tree.getroot()
+        data = {}
+        for trip in root:
+            data[trip.attrib['id']] = {'depart' : trip.attrib['depart'], 'from' : trip.attrib['from'], 'to' : trip.attrib['to']}
+        return data
 
+    def find_trip_id(self, fromLane, toLane):
+        for trip in self.data_trips:
+            if self.data_trips[trip]['from'] == fromLane and self.data_trips[trip]['to'] == toLane:
+                return trip
+        return None
 
-# for sensor_dict in all_data:
-#     coordinates = list(reversed(sensor_dict['geometry']['coordinates']))
-#     sensor_name = sensor_dict['properties']['traverse_name']
-#     if sensor_name == 'STE_TD3':
-#         print('here')
+    def getGeocoordinates(self, coord):
+        '''
+        Returns the geocoordinates of the sumo network coordinates
+        '''
+        #geocoordinates = []
+        point = Point(coord)
+        lon, lat = net.convertXY2LonLat(point.x, point.y)
+        return str(lat) + "," + str(lon)
+        #geocoordinates.append((lat, lon))
+        #return geocoordinates
+    
+    def OD_lists():
+        self.origins=[]
+        self.destinations=[]
+        for trip in data:
+            fromShape = net.getEdge(data[trip]['from']).getShape()
+            toShape = net.getEdge(data[trip]['to']).getShape()
+            
+            self.origins.append( getGeocoordinates(fromShape[0]))
+            self.destinations.append( getGeocoordinates(toShape[0]))
 
-#     if sensor_name in cls.unavailable_sensors or \
-#             (not coordinates[0] or not coordinates[1]) or \
-#             (not np.isfinite(coordinates[0]) or not np.isfinite(coordinates[1])):
-#         continue
-#     record = pd.DataFrame([{'id': sensor_name, 'lat': coordinates[0], 'lon': coordinates[1]}])
-#     df_out = pd.concat([df_out, record], axis=0, ignore_index=True)
-# df_out.to_csv(output_fname, mode='w', sep=';', index=False, header=True)
+        print(self.origins)
+        print(self.destinations)
+    
+    def get_google_data(self, origins, destinations):
+        params = {
+            'origins': origins[:5],
+            'destinations':destinations[:5],
+            'key': API_KEY
+        }
+
+        response = requests.get(endpoint, params=params)
+
+        if response.status_code == 200:
+            data = response.json()
+            rows = data['rows']
+            for row in rows:
+                elements = row['elements']
+                for element in elements:
+                    distance = element['distance']['text']
+                    duration = element['duration']['text']
+                    print(f"Distance: {distance}")
+                    print(f"Duration: {duration}")
+        else:
+            print("fail")
+    
+    
